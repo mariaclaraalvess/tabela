855ea528463df3ee7809dda14e944ff20108280a
mariaclaraalvess <mrclrcarvalhoalves@gmail.com>
2023-07-17 14:54:09 -0300

    Mensagem do commit

--- src/app/user/listar-user/listar-user.component.ts
+++ src/app/user/listar-user/listar-user.component.ts
@@ -1,41 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
 import { UserService } from '../services/user.service';
 import { User } from 'src/app/shared/models/user.model';
-
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalUserComponent } from '../modal-user/modal-user.component';
 import { ModalnewUserComponent } from '../modalnew-user/modalnew-user.component';
+import { USUARIOS } from '../mock-data';
 
 @Component({
   selector: 'app-listar-user',
   templateUrl: './listar-user.component.html',
   styleUrls: ['./listar-user.component.css'],
 })
-
 export class ListarUserComponent implements OnInit {
-
   users: User[] = [];
   userUsername: string = '';
   usuarioEncontrado: User | undefined;
+  usuarios: User[] = [];
 
-  constructor(private userService : UserService,
-              private modalService: NgbModal,
-              ) { }
+  constructor(
+    private userService: UserService,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
-    this.users = this.listarTodos();
+    this.listarUsuarios();
   }
 
-  listarTodos(): User[] {
-    return this.userService.listarTodos();
+  listarUsuarios(): void {
+    this.users = this.userService.listarTodos();
+    this.usuarios = [...this.users, ...USUARIOS];
   }
 
-  remover ($event: any, user: User): void {
+  remover($event: any, user: User): void {
     $event.preventDefault();
     if (confirm(`Deseja realmente remover o usuário ${user.username}?`)) {
-        this.userService.remover(user.id!);
-        this.users = this.listarTodos();
+      this.userService.remover(user.id!);
+      this.listarUsuarios();
     }
   }
 
@@ -46,12 +46,29 @@ export class ListarUserComponent implements OnInit {
 
   abrirModalnewUser(user: User) {
     const modalRef = this.modalService.open(ModalnewUserComponent);
+    modalRef.result.then((result: User | undefined) => {
+      if (result) {
+        if (!result.id) {
+          result.id = this.getNextId();
+        }
+        this.users.push(result);
+        this.listarUsuarios();
+      }
+    });
   }
+  
+  getNextId(): number {
+    const maxId = Math.max(...this.users.map((user) => user.id ?? 0));
+    return maxId > 0 ? maxId + 1 : 1;
+  }
+  
 
   pesquisarUsuario() {
-    this.userService.buscarPorUsername(this.userUsername)
-      .subscribe((usuario: User | undefined) => {
+    this.userService.buscarPorUsername(this.userUsername).subscribe(
+      (usuario: User | undefined) => {
         this.usuarioEncontrado = usuario;
-      });
-    }
+      }
+    );
+  }
+
 }
---
+++ src/app/user/mock-data.ts
@@ -0,0 +1,29 @@
+export const USUARIOS = [
+    {
+      id: 1,
+      email: 'user1@email.com',
+      username: 'teste1'
+    },
+    {
+      id: 2,
+      email: 'user2@email.com',
+      username: 'teste2'
+    },
+    {
+      id: 3,
+      email: 'user3@email.com',
+      username: 'teste3'
+    },
+    {
+      id: 4,
+      email: 'user4@email.com',
+      username: 'teste4'
+    },
+    {
+      id: 5,
+      email: 'user5@email.com',
+      username: 'teste5'
+    }
+  ];
+  
+
